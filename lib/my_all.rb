b3a9cd6065c94573b5e4fbec4a3de0a0416df793
Daniel-Mack <daniel.mack@powerhrg.com>
2018-05-11 10:51:55 -0400

    Followed code-along

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+  block = 0
+  block_return_values = []
+  while block < collection.length
+    block_return_values << yield(collection[block])
+    block += 1
+end
 
+if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
